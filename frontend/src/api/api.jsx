0dc46eb5c1a76f9e5c295d24117375ddfae71eff
rajveersingh097 <rajveersingh002b@gmail.com>
2025-08-14 13:48:32 +0530

    Update api.jsx

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,7 @@
 // src/api/api.jsx
 import axios from "axios";
 const api = axios.create({
-baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+baseURL: import.meta.env.VITE_API_URL || "https://blog-app-3-bs6l.onrender.com",
 });
 // Attach token automatically if present
 api.interceptors.request.use((config) => {
